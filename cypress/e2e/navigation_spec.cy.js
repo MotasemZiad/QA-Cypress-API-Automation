a7de3cdec0633ab00de2ff1da731fc14b658c9f8
Motasem Abunima <motasemabunima@gmail.com>
2023-09-07 09:12:48 +0300

    refactoring...

--- cypress/e2e/navigation_spec.cy.js
+++ cypress/e2e/navigation_spec.cy.js
@@ -6,10 +6,22 @@ describe("Navigation on both Web and Mobile", () => {
   });
 
   it("The navigation bar should be visible", () => {
-    cy.get(".header .content-links").should("be.visible");
+    visibilityOfNavigation("be.visible");
     cy.viewport("iphone-x");
-    cy.get(".header .content-links").should("not.be.visible");
-    cy.get(".hamburger").click();
-    cy.get(".header .content-links").should("be.visible");
+    visibilityOfNavigation("not.be.visible");
+    getHamburger().click();
+    visibilityOfNavigation("be.visible");
   });
 });
+
+function getNavigationLinks() {
+  return cy.get(".header .content-links");
+}
+
+function getHamburger() {
+  return cy.get(".header .wrap .hamburger");
+}
+
+function visibilityOfNavigation(visibility) {
+  getNavigationLinks().should(visibility);
+}
